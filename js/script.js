2d5e353b161a9bb499d7a727c35d9736b19a77c0
Muna Rai <raimuna669@gmailcom>
2021-09-24 13:06:36 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// $(document).on('ready', function () {
+//     $('#client-slider').slick({
+//         dots: true,
+//         arrows: false,
+//         autoplay: true,
+//     });
+// })
+
+
+// $('.service-item') .isotope({
+//     itemselector: '.service-img-box',
+//     layoutMode: 'fitRows'
+// });
+// $('.service-nav-list .service-item li') .click(function(){
+//     $('.service-nav-list .service-item li'). removeClass('active');
+//     $(this) .addClass('active');
+
+//     var selector = $(this) .attr('data-filter');
+//     $('.service-nav-list') .isotope({
+//         filter: selector
+//     });
+//     return false;
+// });
+
+// isotope
+// $(window).on('load', function () {
+//     var $grid = $('.grid').isotope({
+//         itemSelector: '.grid-item',
+//     });
+
+//     $('.filter li').on("click", function () {
+//         $(this).addClass('active');
+//         $(this).siblings().removeClass('active');
+
+//         var value = $(this).attr('data-name');
+//         $grid.isotope({
+//             filter: value
+//         })
+//     })
+
+// })
+
